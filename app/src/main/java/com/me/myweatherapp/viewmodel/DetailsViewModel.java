aeccd1c3e19687da2b967df5a79cb4e98c19d83b
OnTrack Developer <mahmoud_benmoussa@yahoo.fr>
2018-12-24 13:04:07 +0100

    update

--- app/src/main/java/com/me/myweatherapp/viewmodel/DetailsViewModel.java
+++ app/src/main/java/com/me/myweatherapp/viewmodel/DetailsViewModel.java
@@ -51,6 +51,7 @@ public class DetailsViewModel extends ViewModel {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
                     }
                 });
 
